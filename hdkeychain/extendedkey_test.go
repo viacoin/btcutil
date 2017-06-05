a259eaf2ec1b54653cdd67848a41867f280797ee
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-06-05 16:55:44 -0700

    multi: switch imports back to roasbeef

--- hdkeychain/extendedkey_test.go
+++ hdkeychain/extendedkey_test.go
@@ -15,8 +15,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/roasbeef/btcd/chaincfg"
+	"github.com/roasbeef/btcutil/hdkeychain"
 )
 
 // TestBIP0032Vectors tests the vectors provided by [BIP32] to ensure the
